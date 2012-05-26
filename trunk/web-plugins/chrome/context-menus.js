127ea9630a3c2747bd3b48e75c5ea9dbeead943b
tantrieuf31@gmail.com <tantrieuf31@gmail.com@d083e721-3d1a-8887-316c-6dd168c7a44b>
2012-05-26 18:11:43 +0000

    i2tree alpha 8 + fix bugs (add link or images) chrome ext for info node collector

--- trunk/web-plugins/chrome/context-menus.js
+++ trunk/web-plugins/chrome/context-menus.js
@@ -1,19 +1,23 @@
+
+var getSelectedHandler = function(response) {		
+	var data = JSON.parse(response.data);
+	var selectedHtml = data.html;	
+	var title = data.title;
+	var keywords = data.keywords;
+	var viewTabUrl = [ chrome.extension.getURL('knowledge-tree.html') ].join('');
+	chrome.tabs.create({url : viewTabUrl }, function(tab2) {					
+		chrome.tabs.sendRequest(tab2.id, {'html': selectedHtml, 'title': title, 'keywords' : keywords });
+	});	
+};
+
 function genericOnClick(info, tab) {
   //alert("item " + JSON.stringify(info) + " was clicked");
 	chrome.tabs.getSelected(null, function(tab) {			
 		chrome.tabs.sendRequest(tab.id, {
 			method : "getSelectedHtml"				
-		}, function(response) {		
-			var data = JSON.parse(response.data);
-			var selectedHtml = data.html;
-			var name = data.name;
-			var title = data.title;
-			var keywords = data.keywords;
-			var viewTabUrl = [ chrome.extension.getURL('knowledge-tree.html') ].join('');
-			chrome.tabs.create({url : viewTabUrl }, function(tab2) {					
-				chrome.tabs.sendRequest(tab2.id, {'html': selectedHtml, 'name' : name, 'title': title, 'keywords' : keywords });
-			});	
-		});
+		}, getSelectedHandler);
 	});	
 }
-chrome.contextMenus.create({"title": "Add to my tree", "contexts":[ "selection", "link", "image"],"onclick": genericOnClick});
+var contexts = [ "selection", "link", "image"];
+//var contexts = [ "selection" ];
+chrome.contextMenus.create({"title": "Add to my tree", "contexts" : contexts,"onclick": genericOnClick});
